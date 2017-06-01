7bca8f78ec5a666d17eeefd5e08105045d9a10cf
Edson Simões Boldrini <edsonsboldrini@gmail.com>
2017-06-01 01:00:37 -0300

    Add files via upload

--- modelo_fisico.sql
+++ modelo_fisico.sql
@@ -3,25 +3,99 @@
 
 
 
-CREATE TABLE contato_tipo (
-idcontato_tipo VARCHAR(10) PRIMARY KEY,
-descricao VARCHAR(60)
+CREATE TABLE tipo_contato (
+descricao VARCHAR(60),
+idtipo_contato SERIAL PRIMARY KEY
 )
 
-CREATE TABLE responsavel_contato (
-contato VARCHAR(200),
-principal INTEGER,
-idresponsavel_contato VARCHAR(10) PRIMARY KEY,
-idcontato_tipo VARCHAR(10),
-idresponsavel VARCHAR(10),
-FOREIGN KEY(idcontato_tipo) REFERENCES contato_tipo (idcontato_tipo)
+CREATE TABLE aluno (
+codigo VARCHAR(20),
+matricula VARCHAR(20),
+nome VARCHAR(100),
+idaluno SERIAL PRIMARY KEY,
+idendereco INTEGER
 )
 
 CREATE TABLE responsavel (
-nome VARCHAR(10),
-receber_notificacao VARCHAR(10),
-idresponsavel VARCHAR(10) PRIMARY KEY,
+nome VARCHAR(100),
+receber_notificacao INT,
+idresponsavel SERIAL PRIMARY KEY,
 idendereco INTEGER
+)
+
+CREATE TABLE entrada_aluno (
+hora VARCHAR(8),
+data DATE,
+identrada_aluno SERIAL PRIMARY KEY,
+idaluno INTEGER,
+FOREIGN KEY(idaluno) REFERENCES aluno (idaluno)
+)
+
+CREATE TABLE cidade (
+idcidade SERIAL PRIMARY KEY,
+nome VARCHAR(100)
+)
+
+CREATE TABLE curso (
+idcurso SERIAL PRIMARY KEY,
+nome VARCHAR(100),
+descricao VARCHAR(200)
+)
+
+CREATE TABLE turma (
+idturma SERIAL PRIMARY KEY,
+nome VARCHAR(100),
+descricao VARCHAR(200),
+idcurso INTEGER,
+FOREIGN KEY(idcurso) REFERENCES curso (idcurso)
+)
+
+CREATE TABLE horario (
+idhorario SERIAL PRIMARY KEY,
+dia_semana INTEGER,
+idturma INTEGER,
+FOREIGN KEY(idturma) REFERENCES turma (idturma)
+)
+
+CREATE TABLE matricula (
+data DATE,
+matriculado INTEGER,
+idmatricula SERIAL PRIMARY KEY,
+idturma INTEGER,
+idaluno INTEGER,
+FOREIGN KEY(idturma) REFERENCES turma (idturma),
+FOREIGN KEY(idaluno) REFERENCES aluno (idaluno)
+)
+
+CREATE TABLE contato (
+contato VARCHAR(200),
+principal INTEGER,
+idcontato SERIAL PRIMARY KEY,
+idtipo_contato INTEGER,
+idresponsavel INTEGER,
+FOREIGN KEY(idtipo_contato) REFERENCES tipo_contato (idtipo_contato),
+FOREIGN KEY(idresponsavel) REFERENCES responsavel (idresponsavel)
+)
+
+CREATE TABLE data_evento (
+data DATE,
+iddata_evento SERIAL PRIMARY KEY,
+idevento INTEGER
+)
+
+CREATE TABLE entrada_saida (
+horario_saida VARCHAR(6),
+horario_entrada VARCHAR(6),
+identrada_saida INTEGER PRIMARY KEY,
+idhorario INTEGER,
+iddata_evento INTEGER,
+FOREIGN KEY(idhorario) REFERENCES horario (idhorario),
+FOREIGN KEY(iddata_evento) REFERENCES data_evento (iddata_evento)
+)
+
+CREATE TABLE evento (
+idevento SERIAL PRIMARY KEY,
+nome VARCHAR(100)
 )
 
 CREATE TABLE bairro (
@@ -29,71 +103,24 @@ idbairro INTEGER PRIMARY KEY,
 nome VARCHAR(100)
 )
 
-CREATE TABLE cidade (
-idcidade VARCHAR(10) PRIMARY KEY,
-nome VARCHAR(100)
-)
-
 CREATE TABLE endereco (
-idendereco INTEGER PRIMARY KEY,
-endereo VARCHAR(200),
+endereco VARCHAR(200),
 cep VARCHAR(9),
 numero VARCHAR(20),
+idendereco SERIAL PRIMARY KEY,
 idbairro INTEGER,
-idcidade VARCHAR(10),
+idcidade INTEGER,
 FOREIGN KEY(idbairro) REFERENCES bairro (idbairro),
 FOREIGN KEY(idcidade) REFERENCES cidade (idcidade)
 )
 
-CREATE TABLE curso (
-descricao VARCHAR(60),
-idcurso INTEGER PRIMARY KEY
-)
-
-CREATE TABLE turma (
-idturma INTEGER PRIMARY KEY,
-descricao VARCHAR(60),
-hora_saida2 VARCHAR(10),
-hora_inicio1 VARCHAR(10),
-hora_saida1 VARCHAR(10),
-hora_inicio2 VARCHAR(10),
-idcurso INTEGER,
-FOREIGN KEY(idcurso) REFERENCES curso (idcurso)
-)
-
-CREATE TABLE aluno_log (
-hora TIME,
-idaluno_log VARCHAR(10) PRIMARY KEY,
-data DATETIME,
-idaluno VARCHAR(10)
-)
-
-CREATE TABLE aluno_turma (
-matriculado INTEGER,
-data DATETIME,
-idaluno_turma VARCHAR(10) PRIMARY KEY,
-idturma INTEGER,
-idaluno VARCHAR(10),
-FOREIGN KEY(idturma) REFERENCES turma (idturma)
-)
-
-CREATE TABLE aluno (
-idaluno VARCHAR(10) PRIMARY KEY,
-codigo VARCHAR(20),
-matricula VARCHAR(20),
-nome VARCHAR(100),
-idendereco INTEGER,
-FOREIGN KEY(idendereco) REFERENCES endereco (idendereco)
-)
-
 CREATE TABLE aluno_responsavel (
-idaluno VARCHAR(10),
-idresponsavel VARCHAR(10),
+idaluno INTEGER,
+idresponsavel INTEGER,
 FOREIGN KEY(idaluno) REFERENCES aluno (idaluno),
 FOREIGN KEY(idresponsavel) REFERENCES responsavel (idresponsavel)
 )
 
-ALTER TABLE responsavel_contato ADD FOREIGN KEY(idresponsavel) REFERENCES responsavel (idresponsavel)
+ALTER TABLE aluno ADD FOREIGN KEY(idendereco) REFERENCES endereco (idendereco)
 ALTER TABLE responsavel ADD FOREIGN KEY(idendereco) REFERENCES endereco (idendereco)
-ALTER TABLE aluno_log ADD FOREIGN KEY(idaluno) REFERENCES aluno (idaluno)
-ALTER TABLE aluno_turma ADD FOREIGN KEY(idaluno) REFERENCES aluno (idaluno)
+ALTER TABLE data_evento ADD FOREIGN KEY(idevento) REFERENCES evento (idevento)
